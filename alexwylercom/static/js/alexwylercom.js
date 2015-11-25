85c307aef18788284a3a6426b1eca1562ebf9d44
Alex Wyler <alex.s.wyler@gmail.com>
2015-11-25 17:44:58 -0600

    random subtitle, title link

--- alexwylercom/static/js/alexwylercom.js
+++ alexwylercom/static/js/alexwylercom.js
@@ -1,6 +1,32 @@
 (function () {
     'use strict';
 
+    var subtitles = [
+        "I can't name all 150 pokemon.",
+        "Take your shoes off. Stay awhile.",
+        "Can I get you anything? Cofee? Tea?",
+        "Here, let me take your coat.",
+        "How's the fam?",
+        "Hold on, let me grab my phone.",
+        "Just finishing up.  I'll be right out.",
+        "You're just in time for pie!",
+        "Can you get some milk next time you're out?",
+        "My phone's on 5%, so I might lose you.",
+        "How was the drive in?",
+        "Get in! You must be freezing!",
+        "Don't mind the dog.  He's friendly.",
+        "Hungry for apples?",
+        "Wasn't the meeting tomorrow though?",
+        "Don't mind the mess.",
+        "Hey, can you hold this for a second?",
+        "Snape kills dumbledore.",
+        "I had to Spark Notes 'Heart of Darkness'.",
+        "Trying to define yourself is like trying to bite your own teeth.",
+        "Take a left at the next street light.",
+        "Don't use the garbage disposal!",
+        "Thank god.  I'm famished."
+    ];
+
     var app = angular.module('alexwylercom', [
         'ui.router',
         'aw.resolves'
@@ -57,6 +83,9 @@
 
     app.run(function($rootScope, $timeout) {
         $rootScope.date = new Date();
+        $rootScope.randomSubtitle = function() {
+            return subtitles[Math.floor(Math.random()*subtitles.length)];
+        };
 
         $rootScope.$on('$stateChangeSuccess', function() {
             $timeout(medium_embed_onload, 1);
